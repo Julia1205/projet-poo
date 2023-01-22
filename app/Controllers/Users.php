f400a3f340287ad0cd50c555e6d40cb03e53f291
Julia1205 <92686656+Julia1205@users.noreply.github.com>
2023-01-22 17:38:57 +0100

    finished fetchCocktail, started user controller

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -3,6 +3,16 @@
  * @file Users.php
  * @version 1.0.0
  * @brief Controller des utilisateurs
+ * @details
+ * <p>
+ * Cette classe permet de gérer les actions des utilisateurs</p>
+ * <p> Ces actions sont : 
+ * <ul>
+ *  <li><strong>addUser</strong> : page d'inscription
+ *  <li><strong>updateUser</strong> : page de mise des informations de l'utilisateur
+ *  <li><strong>connectUser</strong> : page de connexion
+ *  <li><strong>disconnectUser</strong> : méthode de déconnexion
+ * </ul>
  */
 
  //namespace App\Controllers;
@@ -17,7 +27,8 @@ class Users extends BaseController
 {
     public function __construct()
     {
-        # code...
+        //$CI = & get_instance();
+        $validation =  \Config\Services::validation();
     }
 
     /**
@@ -25,9 +36,36 @@ class Users extends BaseController
      */
     public function addUSer(): void
     {
-        # code...
-        //Redirige vers register
-        $this->display('user/register.tpl');
+        if($this->session('user_id') == NULL){ //checking if the user isn't already connected
+            $this->validation->setRules([
+                'user_email' => 
+                [
+                    'label'  => 'e-mail',
+                    //adding the validation rules
+                    'rules'  => 'required|valid_email|is_unique[user.user_mail]',
+                    //adding the error messages for each validation rule
+                    'errors' => [
+                        'required' => 'Le {field} est obligatoire',
+                        'valid_email' => 'Le {field} doit être au format valide',
+                        'is_unique' => 'Merci d\'utiliser une autre adresse mail',
+                    ],
+                ],
+                'user_password' => 
+                [
+                    'label'  => 'mot de passe',
+                    'rules'  => 'required|max_length[30]|min_length[5]',
+                    'errors' => [
+                        //Attribution des messages d'erreurs individualisés pour chaque condition
+                        'max_length' => 'Le {field} doit être de maximum trente caractères',
+                        'min_length' => 'Le {field} doit être de minimum de cinq caractères',
+                        'required' => 'Le {field} doit être rempli',
+                    ],
+                ],
+            ]);
+        }
+        $this->_data['form_open'] = form_open("/login");
+        $this->_data['form_close'] = form_close();
+        $this->display('user/register.tpl'); //redirecting to the template
     }
 
     /**
@@ -46,6 +84,32 @@ class Users extends BaseController
     public function connectUser(): void
     {
         # code...
+        $arrAttributesUsernameInput = 
+        [
+            'name' => 'username',
+            'id' => 'username',
+            'class' => 'form-control form-control-lg',
+            //'value' => ,
+            'type' => 'text',
+        ];
+        $arrAttributesPasswordInput = 
+        [
+            'name' => 'pwd',
+            'id' => 'pwd',
+            'class' => 'form-control form-control-lg',
+            'type' => 'password',
+        ];
+        $arrAttributesLabel = 
+        [
+            'class' => 'form-label',
+        ];
+        $this->_data['form_open'] = form_open("/login");
+        $this->_data['form_username'] = form_input($arrAttributesUsernameInput);
+        $this->_data['label_username'] = form_label('Your username', 'username', $arrAttributesLabel);
+        $this->_data['form_pwd'] = form_input($arrAttributesPasswordInput);
+        $this->_data['label_password'] = form_label('Your password', 'pwd', $arrAttributesLabel);
+        $this->_data['form_submit' ]= form_submit("submit", "Login", "class='btn btn-purple btn-block btn-lg text-body'");
+        $this->_data['form_close'] = form_close();
         //redirige vers page de connexion
         $this->display('user/login.tpl');
         //Ou home si connexion réussie
