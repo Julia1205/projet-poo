2d8b61e363c2455182946fc077351c66f603a6e3
Julia1205 <92686656+Julia1205@users.noreply.github.com>
2023-01-23 10:00:08 +0100

    update on number of cocktails fetched on home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,7 +9,7 @@ class Home extends BaseController
     public function index()
     {
         $cocktail_model = new Cocktails_model;
-        for ($i=0; $i < 4; $i++) { //fetching 4 random cocktails
+        for ($i=0; $i < 6; $i++) { //fetching 4 random cocktails
             $objCocktail = $cocktail_model->getCocktailByID(rand(1, 400));
         }
         $this->_data['title'] = "Cocktail point";
