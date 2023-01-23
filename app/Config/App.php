9d86a1f6c33f408bf282186b96279b60bfdd5593
Fabian Heilig <fabian68.heilig@gmail.com>
2023-01-23 09:30:43 +0100

    ajout de l'URL de fabian dans app.php et commenté le var_dump de la page d'accueil

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/projet-poo1/public';
+    //public $baseURL = 'http://localhost/projet-poo1/public';
+    public $baseURL = 'http://projet-poo1:8888/'; /*URL fabian*/
 
     /**
      * --------------------------------------------------------------------------
