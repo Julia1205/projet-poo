6ae5e294ae301555bb442cd5427b3f5308a4c3a4
Fabian Heilig <fabian68.heilig@gmail.com>
2023-01-23 11:20:25 +0100

    modification de la gestion du nom des onglets

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -12,21 +12,24 @@ class Home extends BaseController
         for ($i=0; $i < 4; $i++) { //fetching 4 random cocktails
             $objCocktail = $cocktail_model->getCocktailByID(rand(1, 400));
         }
-        $this->_data['title'] = "Cocktail point";
+        $this->_data['title'] = "";
         $this->display('home/home.tpl');
     }
     public function layout(): void
     {
+        $this->_data['title'] = "Layout - ";
         $this->display('layout/layout.tpl');
     }
     /*On accède à la page RGPD*/
     public function rgpd(): void
     {
+        $this->_data['title'] = "GDPR - ";
         $this->display('reglementation/rgpd.tpl');
     }
     /*On accède à la page des CGU*/
     public function gcu(): void
     {
+        $this->_data['title'] = "GCU - ";
         $this->display('reglementation/gcu.tpl');
     }
 }
